cef3676bdc64bc6656d630b80a8b01c6721823bf
minjun0317 <minjjun0317@gmail.com>
2026-09-19 10:37:05 +0900

    chore: watchOS 프로젝트 초기 생성

---
+++ Moil-WOS-V1 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Moil-WOS-V1 Watch App
+//
+//  Created by 민준김 on 9/19/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Moil-WOS-V1 Watch App/Moil_WOS_V1App.swift
@@ -0,0 +1,17 @@
+//
+//  Moil_WOS_V1App.swift
+//  Moil-WOS-V1 Watch App
+//
+//  Created by 민준김 on 9/19/26.
+//
+
+import SwiftUI
+
+@main
+struct Moil_WOS_V1_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
